0ee546387dcd62a554783b369ac4dfa8d5e10d55
Dietrich Ayala <autonome@gmail.com>
2016-05-09 10:29:17 -0700

    viz tests

---
+++ viz/test.js
@@ -0,0 +1,244 @@
+var files = [ 
+  'file-1461594492102-file-1461594490106.jpg.jpg',
+  'file-1461594516256-file-1461594514146.jpg.jpg',
+  'file-1461594531011-file-1461594528759.jpg.jpg',
+  'file-1461605408690-file-1467261313116.jpg.jpg',
+  'file-1461605517620-file-1467261421977.jpg.jpg',
+  'file-1461605660511-file-1467261564960.jpg.jpg',
+  'file-1461606572474-file-1467263582882.jpg.jpg',
+  'file-1461606574594-file-1467263584914.jpg.jpg',
+  'file-1461606575307-file-1467263585552.jpg.jpg',
+  'file-1461606581326-file-1467263591705.jpg.jpg',
+  'file-1461607476963-file-1467263381436.jpg.jpg',
+  'file-1461607476966-file-1467264487320.jpg.jpg',
+  'file-1461607515558-file-1467263420142.jpg.jpg',
+  'file-1461607515678-file-1467264526081.jpg.jpg',
+  'file-1461607525282-file-1467264535554.jpg.jpg',
+  'file-1461607525288-file-1467263429777.jpg.jpg',
+  'file-1461607526299-file-1467263430766.jpg.jpg',
+  'file-1461607526303-file-1467264536548.jpg.jpg',
+  'file-1461607688123-file-1467263592586.jpg.jpg',
+  'file-1461607691751-file-1467264702121.jpg.jpg',
+  'file-1461607748111-file-1467263652626.jpg.jpg',
+  'file-1461607751785-file-1467264762184.jpg.jpg',
+  'file-1461607807998-file-1467263712582.jpg.jpg',
+  'file-1461607811794-file-1467264822115.jpg.jpg',
+  'file-1461607868041-file-1467263772557.jpg.jpg',
+  'file-1461607871804-file-1467264882163.jpg.jpg',
+  'file-1461607928041-file-1467263832625.jpg.jpg',
+  'file-1461607931813-file-1467264942125.jpg.jpg',
+  'file-1461607991839-file-1467265002105.jpg.jpg',
+  'file-1461608048151-file-1467263952674.jpg.jpg',
+  'file-1461608051784-file-1467265062178.jpg.jpg',
+  'file-1461608108242-file-1467264012748.jpg.jpg',
+  'file-1461608111826-file-1467265122170.jpg.jpg',
+  'file-1461608168148-file-1467264072649.jpg.jpg',
+  'file-1461608171762-file-1467265182172.jpg.jpg',
+  'file-1461608228056-file-1467264132640.jpg.jpg',
+  'file-1461608231844-file-1467265242150.jpg.jpg',
+  'file-1461608288082-file-1467264192666.jpg.jpg',
+  'file-1461608351787-file-1467265362188.jpg.jpg',
+  'file-1461608408183-file-1467264312653.jpg.jpg',
+  'file-1461608411753-file-1467265422107.jpg.jpg',
+  'file-1461608467969-file-1467264372565.jpg.jpg',
+  'file-1461608471863-file-1467265482141.jpg.jpg',
+  'file-1461608528105-file-1467264432691.jpg.jpg',
+  'file-1461608531674-file-1467265542090.jpg.jpg',
+  'file-1461608588111-file-1467264492704.jpg.jpg',
+  'file-1461608591769-file-1467265602066.jpg.jpg',
+  'file-1461608648018-file-1467264552597.jpg.jpg',
+  'file-1461608651677-file-1467265662044.jpg.jpg',
+  'file-1461608707997-file-1467264612564.jpg.jpg',
+  'file-1461608711697-file-1467265722057.jpg.jpg',
+  'file-1461608768217-file-1467264672686.jpg.jpg',
+  'file-1461608771794-file-1467265782189.jpg.jpg',
+  'file-1461608828116-file-1467264732625.jpg.jpg',
+  'file-1461608831796-file-1467265842110.jpg.jpg',
+  'file-1461608888125-file-1467264792696.jpg.jpg',
+  'file-1461608891808-file-1467265902110.jpg.jpg',
+  'file-1461608951684-file-1467265962090.jpg.jpg',
+  'file-1461609011711-file-1467266022121.jpg.jpg',
+  'file-1461609071726-file-1467266082121.jpg.jpg',
+  'file-1461609131730-file-1467266142105.jpg.jpg',
+  'file-1461609191740-file-1467266202134.jpg.jpg',
+  'file-1461609251746-file-1467266262068.jpg.jpg',
+  'file-1461609311684-file-1467266322093.jpg.jpg',
+  'file-1461609368107-file-1467265272707.jpg.jpg',
+  'file-1461609428014-file-1467265332604.jpg.jpg',
+  'file-1461609488074-file-1467265392652.jpg.jpg',
+  'file-1461609548094-file-1467265452583.jpg.jpg',
+  'file-1461609608203-file-1467265512676.jpg.jpg',
+  'file-1461609667945-file-1467265572552.jpg.jpg',
+  'file-1461609728013-file-1467265632556.jpg.jpg',
+  'file-1461609788127-file-1467265692614.jpg.jpg',
+  'file-1461609848039-file-1467265752513.jpg.jpg',
+  'file-1461609908146-file-1467265812640.jpg.jpg',
+  'file-1461609967963-file-1467265872551.jpg.jpg',
+  'file-1461610027968-file-1467265932564.jpg.jpg',
+  'file-1461610088040-file-1467265992620.jpg.jpg',
+  'file-1461610148054-file-1467266052586.jpg.jpg',
+  'file-1461610268082-file-1467266172677.jpg.jpg',
+  'file-1461610388085-file-1467266292588.jpg.jpg',
+  'file-1461610928070-file-1467266832674.jpg.jpg',
+  'file-1461610988051-file-1467266892604.jpg.jpg',
+  'file-1461611108065-file-1467267012670.jpg.jpg',
+  'file-1461611168070-file-1467267072623.jpg.jpg',
+  'file-1461611588073-file-1467267492684.jpg.jpg',
+  'file-1461611648131-file-1467267552626.jpg.jpg',
+  'file-1461611708146-file-1467267612620.jpg.jpg',
+  'file-1461611768033-file-1467267672574.jpg.jpg',
+  'file-1461611828143-file-1467267732630.jpg.jpg',
+  'file-1461611888049-file-1467267792541.jpg.jpg',
+  'file-1461611948058-file-1467267852664.jpg.jpg',
+  'file-1461612008176-file-1467267912686.jpg.jpg',
+  'file-1461612068173-file-1467267972718.jpg.jpg'
+];
+
+//var itemCount = files.length,
+var itemCount = 2,
+    fxCount = 3,
+    imgs = []
+
+function setup() {
+  var container = document.querySelector('#container');
+
+  var r = randomInRangeNoRepeat(0, files.length - 1);
+
+  for (var i = 0; i < itemCount; i++) {
+    var file = files[r()];
+    var div = document.createElement('div');
+    var img = document.createElement('img');
+    img.src = 'http://localhost/trashbeauty/uploads/' + file;
+    img.dataset['file'] = file;
+    div.appendChild(img);
+    container.appendChild(div);
+    convertImg(img, fxCount);
+  }
+
+  var cs = document.querySelectorAll('#container > div');
+  for (var i = 0; i < cs.length; i++) {
+    var d = cs[i];
+    d.style.position = 'absolute';
+    d.style.top = window.height/2 - d.height/2;
+    d.style.left = window.width/2 - d.width/2;
+  }
+}
+document.addEventListener('DOMContentLoaded', setup);
+
+function draw() {
+  var width = window.width,
+      height = window.height,
+      startX = width/2,
+      startY = height/2,
+      angle = 0,
+      step = Math.PI * 2 / itemCount;
+
+  imgs.forEach(function(img, i) {
+    var x = startX + 100 * Math.cos( angle );
+    var y = startY + 100 * Math.sin( angle );
+    var r = 360 / i - 1;
+
+    console.log('i', i, 'r', r, 'step', step, 'angle', angle)
+      
+    //var imageIndex = randomIntInRange(1, itemCount);
+      
+    // image(img,[sx=0],[sy=0],[sWidth=img.width],[sHeight=img.height],[dx=0],[dy=0],[dWidth],[dHeight])
+    //
+    // Displays the image at point (0, height/2) at half size
+    //
+    image(img, 180 / i, 180 / i)
+
+    angle += step;
+      
+  })
+}
+
+function convertImg(img, numEffects) {
+  img.onload = function() {
+    var tmpCanvas = document.createElement('canvas'),
+        tmpContext = tmpCanvas.getContext('2d'),
+        outCanvas = document.createElement('canvas'),
+        outContext = outCanvas.getContext('2d'),
+        height = img.height,
+        width = img.width,
+        numEffects = numEffects || 3; // randomIntInRange(1, 3);
+
+    img.parentNode.appendChild(outCanvas);
+
+    tmpCanvas.width = width;
+    tmpCanvas.height = height;
+    outCanvas.width = width;
+    outCanvas.height = height;
+
+    tmpContext.drawImage(img, 0, 0);
+
+    var imgData = tmpContext.getImageData(0, 0, img.width, img.height);
+    //var newImageData = grafi.contrast(grafi.invert(grafi.contrast(grafi.grayscale(imgData, {mode: 'simple', channel: 0}), {level: 2}), {level: 5});
+    var newImageData = null,
+        appliedEffects = [];
+    while (appliedEffects.length != numEffects) {
+      var effect = randomEffect();
+      if (appliedEffects.indexOf(effect) == -1) {
+        newImageData = effect.f(imgData);
+        appliedEffects.push(effect);
+      }
+    }
+    var names = appliedEffects.map(function(e) { return e.name; });
+    outContext.putImageData(newImageData, 0, 0);
+    outCanvas.style.opacity = 0.5;
+
+    var parentNode = img.parentNode;
+    img.remove();
+
+    var label = document.createElement('div');
+    label.textContent = '/' + names.join(', ') + '/';
+    label.classList.add('label'); 
+    parentNode.appendChild(label);
+  };
+}
+
+var fx = {
+  blur: { name: 'blur', f: grafi.blur },
+  brightness: { name: 'brightness', f: grafi.brightness },
+  contrast: { name: 'contrast', f: grafi.contrast, defaults: { level: 2 } },
+  despeckle: { name: 'despeckle', f: grafi.despeckle },
+  // broken
+  //dither: { name: 'dither', f: grafi.dither},
+  // just turns black
+  //edge: { name: 'edge', f: grafi.edge},
+  grayscale: { name: 'grayscale', f: grafi.grayscale, defaults: { mode: 'simple', channel: 0 } },
+  invert: { name: 'invert', f: grafi.invert, defaults: { level: 5 } },
+  posterize: { name: 'posterize', f: grafi.posterize },
+  sharpen: { name: 'sharpen', f: grafi.sharpen},
+  solarize: { name: 'solarize', f: grafi.solarize},
+  threshold: { name: 'threshold', f: grafi.threshold},
+  pseudocolor: { name: 'pseudocolor', f: grafi.pseudocolor }
+};
+
+function randomEffect() {
+  var keys = Object.keys(fx);
+  return fx[ keys[randomIntInRange(0, keys.length - 1)] ];
+}
+
+// so many better ways
+function randomInRangeNoRepeat(min, max) {
+  var used = [];
+  return function() {
+    if (used.length == max - min) {
+      console.log('max hit', used.length, max - min)
+      return null;
+    }
+    var rando = randomIntInRange(min, max);
+    while (used.indexOf(rando) != -1) {
+      rando = randomIntInRange(min, max);
+    }
+    used.push(rando);
+    return rando;
+  }
+}
+
+// Pick a card
+function randomIntInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
